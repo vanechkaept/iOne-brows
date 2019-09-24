df22768e462fda25b5d3e648a741091ab9fb679b
vanechkaept <44510629+vanechkaept@users.noreply.github.com>
2019-09-24 09:18:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// var doc=document,
+//     tetx= doc.querySelector("header .main-text"),
+//     hovAll =doc.querySelectorAll(".hov");
+
+
+
+
+// for( var i=0; i<hovAll.length;i++){
+//     hovAll[i].addEventListener("mouseover", function(){
+//         this.style.transform="scale(1.2)";
+//         this.style.zIndex="999";
+//         // this.style.position="absolute";
+//     });
+//     hovAll[i].addEventListener("mouseout", function(){
+//         this.style.transform="scale(1)";
+//         this.style.zIndex="0";
+//     })
+// }
